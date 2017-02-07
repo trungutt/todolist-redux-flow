7e877e7f45e04beee961ac3861260973cbb42364
trungutt <trung.nguyen@adyoulike.com>
2017-02-07 22:52:25 +0100

    chore(logger): setup redux-logger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
 
 import todosReducer from './reducers/reducers';
 import TodoList from './components/TodoList';
 import actionTypes from './actions/actionTypes';
 
 
-const store = createStore(todosReducer);
+const store = createStore(todosReducer, applyMiddleware(createLogger({ collapsed: true })));
 
 store.dispatch({ type: actionTypes.ADD_TODO, text: 'First thing' });
 store.dispatch({ type: actionTypes.ADD_TODO, text: 'Second thing' });
